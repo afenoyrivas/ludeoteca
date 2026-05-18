bec31970267132b0cbd3ea45133b4f069a205d00
Agostina <afenoyrivas@gmail.com>
2026-05-18 14:33:52 -0300

    Initial commit

---
+++ src/main/java/fenoyagostina/ludeoteca/LudeotecaApplication.java
@@ -0,0 +1,13 @@
+package fenoyagostina.ludeoteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudeotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudeotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fenoyagostina/ludeoteca/LudeotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package fenoyagostina.ludeoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudeotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
